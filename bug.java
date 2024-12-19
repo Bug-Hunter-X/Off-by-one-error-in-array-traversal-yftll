3aef6bc04951080e448ad8df340fbf99d1bf5a71
BugHunter-X <askzenai@gmail.com>
2024-12-19 19:22:04 +0100

    Add bug.java file

---
+++ bug.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int i = 0; i <= array.length; i++) {
+            array[i] = i * 2;
+        }
+        System.out.println(array[9]);
+    }
+}
